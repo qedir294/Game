6510fed5e25c51e460056a1b1e18c89d6e5ffe56
qedir ellezov <safarogluasiman@gmail.com>
2020-09-13 19:01:49 +0400

    game copy

---
+++ Game/StaticParams.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    static class StaticParams
+    {
+        public const int N = 20;
+        public const int F = 5;
+    }
+}
